d114dd1f58f7de2a6df6341a2c582cb405509843
Lóránt Meszlényi <meszlenyilorant@gmail.com>
2019-03-13 11:17:35 +0100

    Handy function to calculate a complete Kalman Step.

--- KalmanFilter.jl
+++ KalmanFilter.jl
@@ -2,7 +2,7 @@ module KalmanFilter
 
 using LinearAlgebra, Distributions, Printf
 
-export Kalman, fusion, predict, K
+export Kalman, fusion, predict, K, next
 
 mutable struct Kalman
     A # State transition
@@ -18,7 +18,7 @@ mutable struct Kalman
     u # Control signal
 
     Kalman(A, Q, G, R, x̂, Σ, B, u) = new(A, Q, G, R, x̂, Σ, B, u)
-    Kalman(A, Q, G, R, x̂, Σ) = new(A, Q, G, R, x̂, Σ, I, 1)
+    Kalman(A, Q, G, R, x̂, Σ) = new(A, Q, G, R, x̂, Σ, I, I)
 end
 
 # Kalman Gain
@@ -30,18 +30,31 @@ function K(Σ, G, R)
 end
 
 """
+    next(k::Kalman, y)
+
+Compute a complete Kalman Step.
+"""
+function next(k::Kalman, y)
+    newInstance = deepcopy(k)
+    @show k
+    p = predict(newInstance).state
+    f = fusion(newInstance, y)
+    (model = newInstance, fusioned = f.state, predicted = p, gain = f.gain)
+end
+
+"""
     fusion(k::Kalman, y, [print = false])
 
 Compute the filtered distribution.
 """
 function fusion(k::Kalman, y, print = false)
-    gain = K(k.Σ, k.G, k.R)
-    k.x̂ = k.x̂ + gain * (y - k.G * k.x̂)
-    k.Σ = (I - gain * k.G) * k.Σ * transpose(I - gain * k.G) + gain * k.R * transpose(gain)
+    g = K(k.Σ, k.G, k.R)
+    k.x̂ = k.x̂ + g * (y - k.G * k.x̂)
+    k.Σ = (I - g * k.G) * k.Σ * transpose(I - g * k.G) + g * k.R * transpose(g)
     if print
         @printf "Fusion:  x̂ %2.3f    Σ %2.3f    K %.3f\n" gain k.x̂ k.Σ
     end
-    (state=k.x̂, cov=k.Σ)
+    (state=k.x̂, cov=k.Σ, gain=g)
 end
 
 """
@@ -58,19 +71,19 @@ function predict(k::Kalman, print = false)
     (state=k.x̂, cov=k.Σ)
 end
 
-"""
-    predict(k::Kalman, u[, print = false])
-
-Next prediction if there is a control signal.
-Optional parameter print for debugging.
-"""
-function predict(k::Kalman, u, print = false)
-    k.x̂ = k.A * k.x̂ + k.B * u
-    k.Σ = k.A * k.Σ * transpose(k.A) + k.Q
-    if print
-        @printf "Predict: x̂ %.3f    Σ %.3f\n" k.x̂ k.Σ
-    end
-    (state=k.x̂, cov=k.Σ)
-end
+# """
+#     predict(k::Kalman, u[, print = false])
+#
+# Next prediction if there is a control signal.
+# Optional parameter print for debugging.
+# """
+# function predict(k::Kalman, u, print = false)
+#     k.x̂ = k.A * k.x̂ + k.B * u
+#     k.Σ = k.A * k.Σ * transpose(k.A) + k.Q
+#     if print
+#         @printf "Predict: x̂ %.3f    Σ %.3f\n" k.x̂ k.Σ
+#     end
+#     (state=k.x̂, cov=k.Σ)
+# end
 
 end
