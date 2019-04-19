14a3b4439488d2611abbf0b5e7dff2a7e97244ad
Lóránt Meszlényi <meszlenyilorant@gmail.com>
2019-04-19 21:27:27 +0200

    Removed all unnecessary files

--- 3dpos/udp.jl
+++
@@ -1,9 +0,0 @@
-using Sockets
-
-udpsock = UDPSocket()
-
-bind(udpsock, ip"192.168.178.25", 5555)
-while true
-  println(String(copy(recv(udpsock))))
-end
-
--- non-linear/generate.jl
+++
@@ -1,9 +0,0 @@
-using Plots
-x = collect(0:0.01:7.5)
-f1(x) = x > 5 ? x^2 : -2x+35
-f2(x) = 0.05x^2
-
-plot(xlim = (2.5, 7.5), title = "Verleich zweier nichtlinearer Systeme", dpi = 350)
-plot!(x, f1.(x), label = "Lineare und Quadratische Funktion")
-plot!(x, f2.(x), label = "Polynom 2. Grades")
-png("plot-nichtlineares-system.png")
